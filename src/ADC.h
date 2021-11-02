8976bfc45df0c0bcf9d946f0af8574f9cf986bd0
matiasgarciagit <matias.garcia@mi.unc.edu.ar>
2021-11-02 17:04:22 -0300

    merge

--- src/ADC.h
+++ src/ADC.h
@@ -8,7 +8,21 @@ void confADC(void){
     ADC_EdgeStartConfig(LPC_ADC,1);
     //ADC_IntConfig(LPC_ADC,ADC_ADINTEN0,ENABLE);
     ADC_ChannelCmd(LPC_ADC,ADC_CHANNEL_0,ENABLE);
-    //NVIC_EnableIRQ(ADC_IRQn);
+    NVIC_EnableIRQ(ADC_IRQn);
 
 }
 
+void ADC_IRQHandler(void)
+{
+
+ADC0Value = ((LPC_ADC->ADDR0)>>4) & 0xFFF; //Variable auxiliar para observar el valor del registro de captura
+
+if (ADC0Value<2054){
+LPC_GPIO0->FIOSET = (1<<22);
+}
+else{
+LPC_GPIO0->FIOCLR = (1<<22);
+}
+
+return;
+}
